ee391a8cefe26b8ee81dafa6c9062794f7cdcf62
psidae <akson.rill@gmail.com>
2019-09-15 19:30:39 +0300

    Initial Commit

---
+++ src/main/java/com/psidae/fluffymemory/FluffyMemoryApplication.java
@@ -0,0 +1,13 @@
+package com.psidae.fluffymemory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluffyMemoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FluffyMemoryApplication.class, args);
+	}
+
+}
